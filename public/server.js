eefd5ebff807d5043bc69cde44e28e7c136c3370
Will Richardson <william.hamish@gmail.com>
2017-08-14 15:54:49 +1200

    Reformatting and stuff

--- public/server.js
+++ public/server.js
@@ -11,14 +11,14 @@ var users = [];
  * @param {User} user
  */
 function findOpponent(user) {
-	for (var i = 0; i < users.length; i++) {
-		if (
-			user !== users[i] && 
-			users[i].opponent === null
-		) {
-			new Game(user, users[i]).start();
-		}
-	}
+  for (var i = 0; i < users.length; i++) {
+    if (
+      user !== users[i] && 
+      users[i].opponent === null
+    ) {
+      new Game(user, users[i]).start();
+    }
+  }
 }
 
 /**
@@ -26,7 +26,7 @@ function findOpponent(user) {
  * @param {User} user
  */
 function removeUser(user) {
-	users.splice(users.indexOf(user), 1);
+  users.splice(users.indexOf(user), 1);
 }
 
 /**
@@ -35,16 +35,16 @@ function removeUser(user) {
  * @param {User} user2
  */
 function Game(user1, user2) {
-	this.user1 = user1;
-	this.user2 = user2;
+  this.user1 = user1;
+  this.user2 = user2;
 }
 
 /**
  * Start new game
  */
 Game.prototype.start = function () {
-	this.user1.start(this, this.user2);
-	this.user2.start(this, this.user1);
+  this.user1.start(this, this.user2);
+  this.user2.start(this, this.user1);
 }
 
 /**
@@ -52,31 +52,31 @@ Game.prototype.start = function () {
  * @return {boolean}
  */
 Game.prototype.ended = function () {
-	return this.user1.guess !== GUESS_NO && this.user2.guess !== GUESS_NO;
+  return this.user1.guess !== GUESS_NO && this.user2.guess !== GUESS_NO;
 }
 
 /**
  * Final score
  */
 Game.prototype.score = function () {
-	if (
-		this.user1.guess === GUESS_ROCK && this.user2.guess === GUESS_SCISSORS ||
-		this.user1.guess === GUESS_PAPER && this.user2.guess === GUESS_ROCK ||
-		this.user1.guess === GUESS_SCISSORS && this.user2.guess === GUESS_PAPER
-	) {
-		this.user1.win();
-		this.user2.lose();
-	} else if (
-		this.user2.guess === GUESS_ROCK && this.user1.guess === GUESS_SCISSORS ||
-		this.user2.guess === GUESS_PAPER && this.user1.guess === GUESS_ROCK ||
-		this.user2.guess === GUESS_SCISSORS && this.user1.guess === GUESS_PAPER
-	) {
-		this.user2.win();
-		this.user1.lose();
-	} else {
-		this.user1.draw();
-		this.user2.draw();
-	}
+  if (
+    this.user1.guess === GUESS_ROCK && this.user2.guess === GUESS_SCISSORS ||
+    this.user1.guess === GUESS_PAPER && this.user2.guess === GUESS_ROCK ||
+    this.user1.guess === GUESS_SCISSORS && this.user2.guess === GUESS_PAPER
+  ) {
+    this.user1.win();
+    this.user2.lose();
+  } else if (
+    this.user2.guess === GUESS_ROCK && this.user1.guess === GUESS_SCISSORS ||
+    this.user2.guess === GUESS_PAPER && this.user1.guess === GUESS_ROCK ||
+    this.user2.guess === GUESS_SCISSORS && this.user1.guess === GUESS_PAPER
+  ) {
+    this.user2.win();
+    this.user1.lose();
+  } else {
+    this.user1.draw();
+    this.user2.draw();
+  }
 }
 
 /**
@@ -84,10 +84,10 @@ Game.prototype.score = function () {
  * @param {Socket} socket
  */
 function User(socket) {
-	this.socket = socket;
-	this.game = null;
-	this.opponent = null;
-	this.guess = GUESS_NO;
+  this.socket = socket;
+  this.game = null;
+  this.opponent = null;
+  this.guess = GUESS_NO;
 }
 
 /**
@@ -95,15 +95,15 @@ function User(socket) {
  * @param {number} guess
  */
 User.prototype.setGuess = function (guess) {
-	if (
-		!this.opponent ||
-		guess <= GUESS_NO ||
-		guess > GUESS_SCISSORS
-	) {
-		return false;
-	}
-	this.guess = guess;
-	return true;
+  if (
+    !this.opponent ||
+    guess <= GUESS_NO ||
+    guess > GUESS_SCISSORS
+  ) {
+    return false;
+  }
+  this.guess = guess;
+  return true;
 };
 
 /**
@@ -112,41 +112,41 @@ User.prototype.setGuess = function (guess) {
  * @param {User} opponent
  */
 User.prototype.start = function (game, opponent) {
-	this.game = game;
-	this.opponent = opponent;
-	this.guess = GUESS_NO;
-	this.socket.emit("start");		
+  this.game = game;
+  this.opponent = opponent;
+  this.guess = GUESS_NO;
+  this.socket.emit("start");    
 };
 
 /**
  * Terminate game
  */
 User.prototype.end = function () {
-	this.game = null;
-	this.opponent = null;
-	this.guess = GUESS_NO;
-	this.socket.emit("end");
+  this.game = null;
+  this.opponent = null;
+  this.guess = GUESS_NO;
+  this.socket.emit("end");
 };
 
 /**
  * Trigger win event
  */
 User.prototype.win = function () {
-	this.socket.emit("win", this.opponent.guess);
+  this.socket.emit("win", this.opponent.guess);
 };
 
 /**
  * Trigger lose event
  */
 User.prototype.lose = function () {
-	this.socket.emit("lose", this.opponent.guess);
+  this.socket.emit("lose", this.opponent.guess);
 };
 
 /**
  * Trigger draw event
  */
 User.prototype.draw = function () {
-	this.socket.emit("draw", this.opponent.guess);
+  this.socket.emit("draw", this.opponent.guess);
 };
 
 /**
@@ -154,26 +154,26 @@ User.prototype.draw = function () {
  * @param {Socket} socket
  */
 module.exports = function (socket) {
-	var user = new User(socket);
-	users.push(user);
-	findOpponent(user);
-	
-	socket.on("disconnect", function () {
-		console.log("Disconnected: " + socket.id);
-		removeUser(user);
-		if (user.opponent) {
-			user.opponent.end();
-			findOpponent(user.opponent);
-		}
-	});
+  var user = new User(socket);
+  users.push(user);
+  findOpponent(user);
+  
+  socket.on("disconnect", function () {
+    console.log("Disconnected: " + socket.id);
+    removeUser(user);
+    if (user.opponent) {
+      user.opponent.end();
+      findOpponent(user.opponent);
+    }
+  });
 
-	socket.on("guess", function (guess) {
-		console.log("Guess: " + socket.id);
-		if (user.setGuess(guess) && user.game.ended()) {
-			user.game.score();
-			user.game.start();
-		}
-	});
+  socket.on("guess", function (guess) {
+    console.log("Guess: " + socket.id);
+    if (user.setGuess(guess) && user.game.ended()) {
+      user.game.score();
+      user.game.start();
+    }
+  });
 
-	console.log("Connected: " + socket.id);
+  console.log("Connected: " + socket.id);
 };
