ee47166f4723d962cb9ad1f84f0675b5c77a965a
Logan Glasson <logan.glasson@gmail.com>
2017-08-27 16:23:54 +1200

    Begin tidying up client.js

--- public/client.js
+++ public/client.js
@@ -1,23 +1,22 @@
-let
-  socket,
-  canvas,
-  elem,
-  me,
-  mothership,
-  selection_start,
-  selection_end,
-  selected;
+(function() {
+  let
+    socket,
+    ctx,
+    me,
+    mothership,
+    selection_start,
+    selection_end,
+    selected;
 
-let draw = () => {
-  canvas.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
-  canvas.strokeStyle = 'white';
-  if(selection_start !== null && selection_end !== null) canvas.strokeRect(...selection_start, ...add(selection_end, inv(selection_start)));
-  Object.values(units).forEach((unit) => unit.draw(canvas));
-  window.requestAnimationFrame(draw);
-}
+  let draw = () => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.strokeStyle = 'white';
+    if(selection_start !== null && selection_end !== null) ctx.strokeRect(...selection_start, ...add(selection_end, inv(selection_start)));
+    Object.values(units).forEach((unit) => unit.draw(ctx));
+    window.requestAnimationFrame(draw);
+  }
 
-bind = () => {
-  socket.on('tick', (commands) => {
+  function handle_tick(commands) {
     //console.log('tick', commands);
     for (var [destination, ...params] of commands) {
       units[destination].receive(...params);
@@ -25,86 +24,74 @@ bind = () => {
     for (var unit of Object.values(units)) {
       unit.tick();
     }
-  });
+  }
 
-  selection_start = null;
-  selection_end = null;
+  init = () => {
+    socket = io({ upgrade: false, transports: ["websocket"] });
+    let elem = document.getElementById('c');
+    ctx = elem.getContext('2d');
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    socket.on('tick', handle_tick);
 
-  elem.addEventListener('contextmenu', (event) => {
-    event.preventDefault();
-  });
+    selection_start = null;
+    selection_end = null;
 
-  elem.addEventListener('mousedown', (event) => {
-    //socket.emit('command', [0, 'move_to', [event.x, event.y]]);
-    if(event.button === 0) {
-      selection_start = [event.x, event.y];
-    }
-  });
+    elem.addEventListener('contextmenu', (event) => { event.preventDefault(); });
 
-  elem.addEventListener('mousemove', (event) => {
-    if(event.button === 0) {
-      selection_end = [event.x, event.y];
-    }
-  });
+    elem.addEventListener('mousedown', (event) => {
+      if(event.button === 0) {
+        selection_start = [event.x, event.y];
+      }
+    });
 
-  elem.addEventListener('mouseup', (event) => {
-    if(event.button === 0) {
-      selection_end = [event.x, event.y];
-      if(selection_start === null) selection_start = selection_end;
-      let tl_x = Math.min(selection_start[0], selection_end[0]);
-      let tl_y = Math.min(selection_start[1], selection_end[1]);
-      let br_x = Math.max(selection_start[0], selection_end[0]);
-      let br_y = Math.max(selection_start[1], selection_end[1]);
-      selection_start = null;
-      selection_end = null;
-      selected = [];
-      let item;
-      for(var unit of Object.values(units)) {
-        if(unit.in_region([tl_x, tl_y], [br_x, br_y])) {
-          selected.push(unit);
-          unit.selected = true;
-        } else {
-          unit.selected = false;
+    elem.addEventListener('mousemove', (event) => {
+      if(event.button === 0) {
+        selection_end = [event.x, event.y];
+      }
+    });
+
+    elem.addEventListener('mouseup', (event) => {
+      if(event.button === 0) {
+        selection_end = [event.x, event.y];
+        if(selection_start === null) selection_start = selection_end;
+        let tl_x = Math.min(selection_start[0], selection_end[0]);
+        let tl_y = Math.min(selection_start[1], selection_end[1]);
+        let br_x = Math.max(selection_start[0], selection_end[0]);
+        let br_y = Math.max(selection_start[1], selection_end[1]);
+        selection_start = null;
+        selection_end = null;
+        selected = [];
+        let item;
+        for(var unit of Object.values(units)) {
+          if(unit.in_region([tl_x, tl_y], [br_x, br_y])) {
+            selected.push(unit);
+            unit.selected = true;
+          } else {
+            unit.selected = false;
+          }
+        }
+      } else if(event.button === 2) {
+        let offset = [0, 0]
+        for(var unit of selected) {
+          socket.emit('command', [unit.id, 'move_to', add([event.x, event.y], offset)]);
+          offset = add(offset, [12, 0]);
         }
       }
-    } else if(event.button === 2) {
-      //let average_pos = [0, 0]
-      //for(unit of selected) {
-        //average_pos = add(average_pos, unit.position);
-      //}
-      //average_pos = scale(average_pos, 1/selected.length);
-      let offset = [0, 0]
-      for(var unit of selected) {
-        socket.emit('command', [unit.id, 'move_to', add([event.x, event.y], offset)]);
-        offset = add(offset, [12, 0]);
+    });
+
+    socket.on('connected', (units) => {
+      console.log('connected', units);
+      for(var unit of units) {
+        new Drone(unit);
       }
-    }
-  });
-  //socket.on('new_unit', (data) => {
-    //units.push(UnitFactory(data));
-    //draw();
-  //});
+    });
 
-  socket.on('connected', (units) => {
-    console.log('connected', units);
-    for(var unit of units) {
-      new Drone(unit);
-    }
-  });
+    socket.on("error", () => {
+      console.log("error")
+    });
+    window.requestAnimationFrame(draw);
+  }
 
-  socket.on("error", () => {
-    console.log("error")
-  });
-}
-
-init = () => {
-  socket = io({ upgrade: false, transports: ["websocket"] });
-  elem = document.getElementById('c');
-  canvas = elem.getContext('2d');
-  canvas.canvas.width = window.innerWidth;
-  canvas.canvas.height = window.innerHeight;
-  bind();
-  window.requestAnimationFrame(draw);
-}
-
-window.addEventListener("load", init, false);
+  window.addEventListener("load", init, false);
+})();
