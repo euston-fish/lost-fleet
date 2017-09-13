dfba14cc0c0f78c5a4128e5bdcb68069fc4e8fdb
Logan Glasson <logan.glasson@gmail.com>
2017-09-13 22:38:07 +1200

    Slightly better stats maybe

--- src/stats.shared.js
+++ src/stats.shared.js
@@ -1,34 +1,34 @@
 let Stats;
 {
-  let norm_scaled_value = (value) => (1/(1-value)-1);
-  let norm_unscaled_value = (value) => (1-1/(value+1));
-
   Stats = function(norm_stats) {
     this.norm_stats = norm_stats;
     this.attack = {
-      range: 60*norm_scaled_value(norm_stats.attack.range),
-      power: 5*norm_scaled_value(norm_stats.attack.power),
-      efficiency: norm_unscaled_value(0.1*norm_scaled_value(norm_stats.attack.efficiency)),
+      range: 1000 * norm_stats.attack.range,
+      power: 15 * norm_stats.attack.power,
+      efficiency: norm_stats.attack.efficiency,
     }
     this.mine = {
-      range: 80*norm_scaled_value(norm_stats.mine.range),
-      power: 7*norm_scaled_value(norm_stats.mine.power),
-      efficiency: norm_unscaled_value(30*norm_scaled_value(norm_stats.mine.efficiency)),
+      range: 500 * norm_stats.mine.range,
+      power: 20 * norm_stats.mine.power,
+      efficiency: norm_stats.mine.efficiency,
     }
     this.construct = {
-      range: 30*norm_scaled_value(norm_stats.construct.range),
-      power: 4*norm_scaled_value(norm_stats.construct.power),
-      efficiency: norm_unscaled_value(40*norm_scaled_value(norm_stats.construct.efficiency)),
+      range: 350 * norm_stats.construct.range,
+      power: 20 * norm_stats.construct.power,
+      efficiency: norm_stats.construct.efficiency,
     }
     this.misc = {
-      acceleration: norm_scaled_value(norm_stats.misc.acceleration),
-      capacity: 300*norm_scaled_value(norm_stats.misc.capacity),
-      defence: 1-norm_unscaled_value(3*norm_scaled_value(norm_stats.misc.defence)),
-      'transfer range': 70*norm_scaled_value(norm_stats.misc['transfer range']),
+      acceleration: 4 * norm_stats.misc.acceleration,
+      capacity: 1000 * norm_stats.misc.capacity,
+      defence: 1-norm_stats.misc.defence,
+      'transfer range': 2000 * norm_stats.misc['transfer range'],
     }
-    this.cost = 100 * norm_stats.attack.range * norm_stats.attack.power * (norm_stats.attack.efficiency + 1) +
-                100 * norm_stats.mine.range * norm_stats.mine.power * norm_stats.mine.efficiency +
-                100 * norm_stats.construct.range * norm_stats.construct.power * norm_stats.construct.efficiency +
+    this.cost = 100 * norm_stats.attack.range * norm_stats.attack.power +
+                500 * norm_stats.attack.efficiency +
+                100 * norm_stats.mine.range * norm_stats.mine.power +
+                100 * norm_stats.mine.efficiency +
+                100 * norm_stats.construct.range * norm_stats.construct.power +
+                150 * norm_stats.construct.efficiency +
                 100 * norm_stats.misc.acceleration +
                 100 * norm_stats.misc.capacity +
                 100 * norm_stats.misc.defence +
