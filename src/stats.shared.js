d61bcba6efdba0edb3620e9303abd66a6f82a8df
Logan Glasson <logan.glasson@gmail.com>
2017-09-13 22:15:14 +1200

    Fix defence direction

--- src/stats.shared.js
+++ src/stats.shared.js
@@ -23,7 +23,7 @@ let Stats;
     this.misc = {
       acceleration: norm_scaled_value(norm_stats.misc.acceleration),
       capacity: 300*norm_scaled_value(norm_stats.misc.capacity),
-      defence: norm_unscaled_value(3*norm_scaled_value(norm_stats.misc.defence)),
+      defence: 1-norm_unscaled_value(3*norm_scaled_value(norm_stats.misc.defence)),
       'transfer range': 70*norm_scaled_value(norm_stats.misc['transfer range']),
     }
     this.cost = 100 * norm_stats.attack.range * norm_stats.attack.power * (norm_stats.attack.efficiency + 1) +
